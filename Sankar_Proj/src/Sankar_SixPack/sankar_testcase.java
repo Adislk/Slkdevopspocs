de317dc300c14283c19ded6349874fe35d33b2eb
Adislk <adi.gudditi@slkgroup.com>
2018-03-20 18:58:37 +0530

    Second commit test case

--- Sankar_Proj/src/Sankar_SixPack/sankar_testcase.java
+++ Sankar_Proj/src/Sankar_SixPack/sankar_testcase.java
@@ -9,7 +9,7 @@ public class sankar_testcase {
 	@Test
 	public void testConcatinate() {
 		Sankar_class test = new Sankar_class();
-				String result = test.concatenate ("abc","efg");
+				String result = test.concatenate ("333","efg");
 		assertEquals ("abcefg",result);
 		System.out.println(result);
 			}
