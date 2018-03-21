b57f8c6f864cfc3e2d8de0ac4dcb81db4c1a87fe
Adislk <36727624+Adislk@users.noreply.github.com>
2018-03-21 11:29:06 +0530

    Update sankar_testcase.java

--- Sankar_Proj/src/Sankar_SixPack/sankar_testcase.java
+++ Sankar_Proj/src/Sankar_SixPack/sankar_testcase.java
@@ -10,7 +10,9 @@ public class sankar_testcase {
 	public void testConcatinate() {
 		Sankar_class test = new Sankar_class();
 				String result = test.concatenate ("aaaaa","efg");
-		assertEquals ("aaaaaefg",result);
+		
+		assertEquals("Result", 0, result.concatenate("5", "0"));
+
 		System.out.println(result);
 			}
 
