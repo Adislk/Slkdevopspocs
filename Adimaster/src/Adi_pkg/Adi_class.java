1d1093e1b210c96c5c2aee00aa0db6daec18a3b0
Adislk <adi.gudditi@slkgroup.com>
2018-03-21 15:15:05 +0530

    Abcd commit test case

---
+++ Adimaster/src/Adi_pkg/Adi_class.java
@@ -0,0 +1,16 @@
+package Adi_pkg;
+
+public class Adi_class {
+	public String concatenate(String one, String two){
+		return one + two;
+			}
+	public int Multiply(int number1, int number2)
+	{
+		return number1 + number2;
+	}
+		public int subtract1(int numbera, int numberb)
+	{
+	     return numbera - numberb;
+	}
+	
+}
